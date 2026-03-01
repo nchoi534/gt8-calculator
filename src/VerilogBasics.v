349deb33c35e30db3eaea3bc1d629bc034fa267c
nchoi534 <cereal1eater@gmail.com>
2026-03-01 18:51:42 -0500

    Stage 4: Half adder module

--- src/VerilogBasics.v
+++ src/VerilogBasics.v
@@ -47,3 +47,13 @@ module mux_2to1 (
         endcase
     end
 endmodule
+
+module half_adder (
+    input wire A,
+    input wire B,
+    output wire sum,
+    output wire carry
+);
+    assign sum = A ^ B; 
+    assign carry = A & B;
+endmodule
